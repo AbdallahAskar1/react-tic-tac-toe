2b03688d88b4f546f7c9f6b96ef1942b6ea8e56e
AbdallahAskar1 <abdoasker.1426@gmail.com>
2024-02-10 05:52:19 +0200

    add gameBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 import Player from "./components/Player"
-
+import GameBoard from "./components/GameBoard"
+import { useState } from "react";
 function App() {
   
-
+  const [activePlayer,setActivePlayer]=useState("X");
+  const [gameTurns,setGameTurns]=useState([])
+  
+  
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((prevp)=>{
+      return prevp === "X"? "O" : "X";
+    });
+    setGameTurns((prev)=>{
+      let currentPlayer = "X";
+      if(prev.length>0 && prev[0].player === "X") {
+        currentPlayer="O"
+      }
+      const updatedTurns = [
+        {square:{row:rowIndex, col:colIndex},player:currentPlayer},
+        ...prev,
+      ]
+      return updatedTurns
+    })
+    
+  }
   return (
     <>
    <div id="game-container">
     <ol id="players">
 
-    <Player name="palyer1" symbol='X'></Player>
-    <Player name="palyer2" symbol='O'></Player>
+    <Player name="palyer1" symbol='X' isActive={activePlayer === "X"}></Player>
+    <Player name="palyer2" symbol='O' isActive={activePlayer === "O"}></Player>
     </ol>
-    GAME BOARD 
+    <GameBoard  onSelectSquare={handleSelectSquare} turns={gameTurns}/>
    </div>
    LOG
     </>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,33 @@
-const initalGameBoard= [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-]
-function GameBoard() {
+
+const initialGameBoard = [
+  [null,null,null],
+  [null,null,null],
+  [null,null,null],
+];
+// eslint-disable-next-line react/prop-types
+function GameBoard( {onSelectSquare,turns}) { 
+let gameBoard = initialGameBoard;
+
+  for(const turn of turns) {
+    const {square,player} = turn;
+    const {row,col}= square;
+    gameBoard[row,col] = player
+  }
   return (
     <ol id="game-board">
-         {initalGameBoard.map((row,indexRow)=>{
-            <li key={indexRow}>
-                {row.map((item,index)=>{
-                    <li key={index}>
-                        <button>{item}</button>
-                    </li>
-                })}
-            </li>
-         })}
+      {gameBoard.map((Row, indexRow) => (
+        <li key={indexRow}>
+          <ol >
+            {Row.map((item, indexCol) => (
+              <li key={indexCol}>
+                <button onClick={()=>onSelectSquare(indexRow,indexCol)}>{item}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
-  )
+  );
 }
 
-export default GameBoard
+export default GameBoard;
